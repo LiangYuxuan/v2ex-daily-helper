4e3ca0eac4fc40efbe9ce2a5e86d886ab305e085
LiangYuxuan <lyx@lyx.es>
2024-11-11 15:34:22 +0800

    feat: change user-agent source, add fraction as factor

--- src/userAgent.ts
+++ src/userAgent.ts
@@ -1,27 +1,30 @@
-/* eslint-disable @typescript-eslint/naming-convention */
-
-interface ChromeVersion {
-    channel: string,
-    chromium_main_branch_position: number,
-    hashes: {
-        angle: string,
-        chromium: string,
-        dawn: string,
-        devtools: string,
-        pdfium: string,
-        skia: string,
-        v8: string,
-        webrtc: string,
-    },
-    milestone: number,
-    platform: string,
-    previous_version: string,
-    time: number,
-    version: string,
+interface ChromeReleaseVersion {
+    releases: {
+        name: string,
+        serving: {
+            startTime: string,
+        },
+        fraction: number,
+        version: string,
+        fractionGroup: string,
+        pinnable: boolean,
+    }[],
+    nextPageToken: string,
 }
 
-const res = await fetch('https://chromiumdash.appspot.com/fetch_releases?channel=Stable&platform=Mac&num=1&offset=0');
-const data = await res.json() as ChromeVersion[];
-const major = data[0].milestone;
+const res = await fetch('https://versionhistory.googleapis.com/v1/chrome/platforms/mac/channels/stable/versions/all/releases?filter=endtime=none');
+const data = await res.json() as ChromeReleaseVersion;
+const mostVersion = data.releases.reduce((prev, curr) => {
+    if (curr.fractionGroup > prev.fractionGroup) {
+        return curr;
+    }
+
+    if (curr.fractionGroup === prev.fractionGroup && curr.fraction > prev.fraction) {
+        return curr;
+    }
+
+    return prev;
+});
+const major = parseInt(mostVersion.version.split('.')[0], 10);
 
 export default `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${major.toString()}.0.0.0 Safari/537.36`;
